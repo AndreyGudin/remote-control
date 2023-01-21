15ca3c680d572865e4dc77f613f2db6c5a6c586d
Andrey Gudin <aeon150791@gmail.com>
2023-01-21 18:41:40 +0400

    feat: add mouse position

--- src/modules/handleMouseMove.ts
+++ src/modules/handleMouseMove.ts
@@ -1,23 +1,28 @@
 import * as WebSocket from "ws";
 import { mouse, left, right, up, down } from "@nut-tree/nut-js";
 
-export default async function handleMouseMove(data: WebSocket.RawData):Promise<string> {
+export default async function handleMouseMove(
+  data: WebSocket.RawData
+): Promise<string> {
   const [direction, px] = data.toString().split(" ");
   switch (direction) {
     case "mouse_right":
       await mouse.move(right(+px));
-      return `${direction} ${px}px`;
+      return `${direction}`;
     case "mouse_left":
       await mouse.move(left(+px));
-      return `${direction} ${px}px`;
+      return `${direction}`;
     case "mouse_up":
       await mouse.move(up(+px));
-      return `${direction} ${px}px`;
+      return `${direction}`;
     case "mouse_down":
       await mouse.move(down(+px));
-      return `${direction} ${px}px`;
+      return `${direction}`;
+    case "mouse_position":
+      const position = await mouse.getPosition();
+      return `${direction} ${position.x},${position.y}`;
     default:
-      console.log('Invalid command');
+      console.log("Invalid command");
       return `Invalid command`;
   }
 }
