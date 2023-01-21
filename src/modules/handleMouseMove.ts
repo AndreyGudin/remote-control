ada31ccd49b9ad0dada36c0074429e98355c7e0f
Andrey Gudin <aeon150791@gmail.com>
2023-01-21 19:19:18 +0400

    feat: add draw rectangle

--- src/modules/handleMouseMove.ts
+++ src/modules/handleMouseMove.ts
@@ -1,29 +1,34 @@
 import * as WebSocket from "ws";
 import { mouse, left, right, up, down } from "@nut-tree/nut-js";
 import drawSquare from "./drawSquare";
+import drawRectangle from "./drawRectangle";
 
 export default async function handleRequest(
   data: WebSocket.RawData
 ): Promise<string> {
-  const [operation, px] = data.toString().split(" ");
+  const [operation, px1, px2] = data.toString().split(" ");
   switch (operation) {
     case "mouse_right":
-      await mouse.move(right(+px));
+      await mouse.move(right(+px1));
       return `${operation}`;
     case "mouse_left":
-      await mouse.move(left(+px));
+      await mouse.move(left(+px1));
       return `${operation}`;
     case "mouse_up":
-      await mouse.move(up(+px));
+      await mouse.move(up(+px1));
       return `${operation}`;
     case "mouse_down":
-      await mouse.move(down(+px));
+      await mouse.move(down(+px1));
       return `${operation}`;
     case "mouse_position":
       const position = await mouse.getPosition();
       return `${operation} ${position.x},${position.y}`;
     case "draw_square":
-      await drawSquare(+px) ;
+      await drawSquare(+px1);
+      return `${operation}`;
+    case "draw_rectangle":
+      await drawRectangle(+px1, +px2);
+      return `${operation}`;
     default:
       console.log("Invalid command");
       return `Invalid command`;
