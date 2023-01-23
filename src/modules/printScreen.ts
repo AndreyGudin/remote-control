6c1950e2cf2f78ac625c4c7eb860ca33a5d9da2e
Andrey Gudin <aeon150791@gmail.com>
2023-01-23 19:21:31 +0400

    feat: add transform image to rgb

--- src/modules/printScreen.ts
+++ src/modules/printScreen.ts
@@ -4,8 +4,9 @@ import Jimp from 'jimp';
 export default async function printScreen():Promise<string>{
   const currentPosition = await mouse.getPosition();
   const screenshot = await screen.grabRegion(new Region(currentPosition.x, currentPosition.y, 200, 200));
-  const image = new Jimp({data:screenshot.data, width: 200, height: 200});
+  const screenshotRGB = await screenshot.toRGB();
+  const image = new Jimp({data:screenshotRGB.data, width: 200, height: 200});
   const string = await image.getBase64Async(Jimp.MIME_PNG);
   const imageToFront = string.replace('data:image/png;base64,', '');
-  return imageToFront
+  return imageToFront;
 }
